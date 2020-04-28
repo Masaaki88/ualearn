23ddf465a1a60d0126e345b1b034483867d08e8f
Masaaki88 <mamurak@yahoo.de>
2020-04-28 11:59:22 +0200

    First version: simple loop over input word pairs, only ua->de

---
+++ main.py
@@ -0,0 +1,65 @@
+import json
+from dataclasses import dataclass, asdict
+from random import sample
+
+
+@dataclass
+class WordPair:
+    index: int
+    ukrainian: str
+    german: str
+    n_correct: int = 0
+    n_asked: int = 0
+
+
+def main():
+    words_file_name = 'data/words.json'
+    stats_file_name = 'data/word_stats.json'
+    
+    word_pairs = read_word_pairs(words_file_name)
+    result_word_pairs = run_questions(word_pairs)
+    write_results(result_word_pairs, stats_file_name)
+
+
+def read_word_pairs(words_file_name):
+    print(f'Reading {words_file_name}.')
+    with open(words_file_name, 'r', encoding='utf-8') as inputfile:
+        raw_word_pairs = json.load(inputfile)    
+    print(f'Read {len(raw_word_pairs)} items.')
+
+    word_pairs = [
+        WordPair(index=i_pair, ukrainian=raw_pair[0], german=raw_pair[1])
+        for i_pair, raw_pair in enumerate(raw_word_pairs)
+    ]
+    return word_pairs
+
+
+def run_questions(word_pairs):
+    pairs_to_ask = list(range(len(word_pairs)))
+
+    print('\nUebersetze folgende Woerter:')
+    while pairs_to_ask:
+        index = sample(pairs_to_ask, 1)[0]
+        pair = word_pairs[index]
+        print(f'\n{pair.ukrainian}')
+        answer = input()
+        if answer == pair.german:
+            print('Korrekt!')
+            pair.n_correct += 1
+            pairs_to_ask.remove(index)
+        else:
+            print(f'Falsch. Die richtige Uebersetzung lautet: {pair.german}')
+        pair.n_asked += 1
+    print('')
+    return word_pairs
+
+
+def write_results(result_word_pairs, stats_file_name):
+    output_word_pairs = [asdict(word_pair) for word_pair in result_word_pairs]
+    with open(stats_file_name, 'w', encoding='utf-8') as outputfile:
+        json.dump(output_word_pairs, outputfile, ensure_ascii=False, indent=2)
+    print(f'Wrote results to {stats_file_name}.')
+
+
+if __name__ == '__main__':
+    main()
